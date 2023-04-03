051dd4a1e317386cd959d5b7020b6e24165414f7
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-03 09:34:01 +0000

    resources:booksにてbooksのルーティング設定済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  #ルートパス設定完了（4/3 18:00）
   get '/' => 'homes#top'
-  
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  
+  resources:books
+  # get '/books' => 'books#new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

ff8dce6f888b583a04dc168fcecb3d2522d2aed5
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-04 07:44:09 +0000

    ファイルの記述・CRUDの実装が完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,5 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+
 
   def create
     book = Book.new(book_params)
@@ -11,6 +9,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,7 +19,13 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
@@ -29,7 +34,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    #require(:book)を消去したらエラー解消（元々bookテーブルに保存されるものとして送信されているためparamsでの記述するとダブるためかな。。）
-    params.permit(:title, :body)
+    #
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/books' => 'books#index'
   get '/books/:id' => 'books#show', as: 'book'
   get '/books/:id/edit' => 'books#edit', as: 'book_edit'
+  patch '/books/:id' => 'books#update', as: 'book_update'
   delete '/books/:id' => 'books#destroy', as: 'book_destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

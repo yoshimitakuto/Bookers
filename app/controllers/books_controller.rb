41f73be3e2f610f6343fb07ac4a666abb7748cd8
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-04 03:34:48 +0000

    課題手順チェックシートのbookのindexページの作成まで一旦完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    #require(:book)を消去したらエラー解消（元々bookテーブルに保存されるものとして送信されているためparamsでの記述するとダブるためかな。。）
+    params.permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   #ルートパス設定完了（4/3 18:00）
   get '/' => 'homes#top'
-  resources:books
+  # resources:books
   # get '/books' => 'books#new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
+  post '/books' => 'books#create'
+  get '/books' => 'books#index'
+  get '/books/:id' => 'books#show', as: 'book'
+  get '/books/:id/edit' => 'books#edit', as: 'book_edit'
+  delete '/books/:id' => 'books#destroy', as: 'book_destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
